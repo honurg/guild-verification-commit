d3dc32692d064ff4965fcc172b34d8e8174f7f68
honurg <06hbg42@gmail.com>
2026-03-06 11:02:27 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:00:09.605Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T08:02:27.088Z
+Commit: Patch API endpoint error handling
