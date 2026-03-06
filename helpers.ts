a63568ee350aa78145ad8b4744288884fe4dd69b
honurg <06hbg42@gmail.com>
2026-03-06 10:21:29 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:08:40.412Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T07:21:28.897Z
+Commit: Resolve timezone issues in date handling
