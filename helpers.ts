791eec8d9a4117d4fabbaec90086f74f4977d6c5
honurg <06hbg42@gmail.com>
2026-03-06 10:42:01 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:36:37.033Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T07:42:01.057Z
+Commit: Patch security vulnerability in authentication
