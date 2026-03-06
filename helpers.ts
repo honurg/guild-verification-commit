91d430d155c3b5f54d26e947f34d28a14f51453b
honurg <06hbg42@gmail.com>
2026-03-06 10:08:40 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T07:08:35.697Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T07:08:40.412Z
+Commit: Fix CSS styling bug on mobile devices
