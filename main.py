96fc7ad13c922c82de8b7fa2ca1c8675a3350ca1
honurg <06hbg42@gmail.com>
2026-03-07 15:43:15 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T08:02:30.796Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-07T12:43:14.856Z
+Commit: Fix broken links in documentation
