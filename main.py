9990a896d8b54cf4e68960f11ca0c9864170141b
honurg <06hbg42@gmail.com>
2026-03-07 15:43:24 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T12:43:22.656Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-07T12:43:24.099Z
+Commit: Fix CSS styling bug on mobile devices
