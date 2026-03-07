f15c208d35af08c0fcb3488a58a48a519a6f1755
honurg <06hbg42@gmail.com>
2026-03-07 15:43:27 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T12:43:21.049Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-07T12:43:26.794Z
+Commit: Fix memory leak in event handlers
